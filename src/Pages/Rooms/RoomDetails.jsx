4f773ab578e627ab5c3c9ecf09777ae5f24d5a24
Mahin Ahmed <mahindipto18@gmail.com>
2023-11-08 15:45:57 +0600

    added onsubmit function to book now button andr and completed ui signn oo room details page

--- src/Pages/Rooms/RoomDetails.jsx
+++ src/Pages/Rooms/RoomDetails.jsx
@@ -1,17 +1,42 @@
-import { useLoaderData } from "react-router-dom";
+import { useContext } from "react";
+import { Link, useLoaderData } from "react-router-dom";
+import { AuthContext } from './../../Providers/AuthProvider';
 
 const RoomDetails = () => {
+    const { user} = useContext(AuthContext);
+    console.log(user)
     const details = useLoaderData()
-    const { img, roomNumber, roomDescription, roomType, description, pricePerNight, roomSize} = details;
+    const {_id, img, roomNumber, roomDescription, roomType, description, pricePerNight, roomSize } = details;
+
+    const handleBooking=event=>{
+        event.preventDefault();
+        const form = event.target;
+        const RoomId = _id
+        const Type = form.type.value;
+        const Number=form.number.value;
+        const Price = form.price.value;
+        const Name = user.displayName;
+        const Email = form.email.value;
+        const Image = img;
+
+        const bookedRoom = {RoomId, Type ,Name, Number, Price, Email, Image }
+
+        console.log(bookedRoom);
+
+    }
     return (
         <div className="w-11/12 mx-auto bg-[#1E2C1A] text-[#D49B35] rounded-lg">
             <h3 className="text-center font-bold text-2xl py-6"> Room Type : {roomType}</h3>
             <div className="p-9">
                 <div className="grid grid-cols-2 py-7">
-                <div >
-                <img className=" h-72 rounded-lg hover:translate-y-4" src={img} alt="" />
+                    <div >
+                        <img className=" h-72 rounded-lg hover:translate-y-4" src={img} alt="" />
+                        <div className="w-80 text-xl py-3">
+                    <h3>{roomDescription}</h3>
+                    <h4>{description}</h4>
                 </div>
-                <div className="w-80">
+                    </div>
+                    <div className="w-80">
                         <table className="table">
                             {/* head */}
                             <thead className="text-xl text-[#D49B35]">
@@ -30,11 +55,56 @@ const RoomDetails = () => {
                                 </tr>
                             </tbody>
                         </table>
+                        <form  onSubmit={handleBooking} >
+                            <div className="form-control md:w-1/2">
+                                <label className="label">
+                                    <span className="label-text">Room Type</span>
+                                </label>
+                                <label className="input-group">
+                                    <input type="text" name="type" defaultValue={roomType} placeholder="Room Type" className="input input-bordered w-full" />
+                                </label>
+                            </div>
+                            <div className="form-control md:w-1/2">
+                                <label className="label">
+                                    <span className="label-text">Date</span>
+                                </label>
+                                <label className="input-group">
+                                    <input type="date" name="date" className="input input-bordered w-full" />
+                                </label>
+                            </div>
+                            <div className="form-control md:w-1/2">
+                                <label className="label">
+                                    <span className="label-text">Price</span>
+                                </label>
+                                <label className="input-group">
+                                    <input type="text" defaultValue={pricePerNight} name="price" placeholder="price" className="input input-bordered w-full" />
+                                </label>
+                            </div>
+                            <div className="form-control md:w-1/2">
+                        <label className="label">
+                            <span className="label-text">Room Number</span>
+                        </label>
+                        <label className="input-group">
+                            <input type="text" name="number" defaultValue={roomNumber} placeholder="Room Number" className="input input-bordered w-full" />
+                        </label>
                     </div>
-                </div>
-                <div className="w-80 text-xl">
-                    <h3>{roomDescription}</h3>
-                    <h4>{description}</h4>
+                            <div className="form-control md:w-1/2">
+                        <label className="label">
+                            <span className="label-text">Email</span>
+                        </label>
+                        <label className="input-group">
+                            <input type="email" name="email" defaultValue={user?.email} placeholder="email" className="input input-bordered w-full" />
+                        </label>
+                    </div>
+                    {
+                        user ?
+                        <input type="submit" value="Book Now" className="font-semibold btn my-5" />
+                        :
+                        <Link to='/login'><input type="button" value="Login" className="font-semibold  btn my-5" /></Link>
+
+                    }
+                        </form>
+                    </div>
                 </div>
             </div>
         </div>

2a49e93d8b40dc66bd253d1de0cc04527ba0d94a
Mahin Ahmed <mahindipto18@gmail.com>
2023-11-09 17:05:46 +0600

    added some designs

--- src/Pages/Rooms/Room.jsx
+++ src/Pages/Rooms/Room.jsx
@@ -2,11 +2,16 @@
 import { PropTypes } from 'prop-types';
 import { Link } from 'react-router-dom';
 const Room = ({room}) => {
-    const { _id, img} = room; 
+    const { _id, img, roomType, roomSize, pricePerNight} = room; 
     return (
       <Link to={`/rooms/${_id}`}>
-        <div className='my-5'>
+        <div className='my-5    text-[#D49B35] border-x-2 border-y-2 rounded-lg p-2'>
             <img className='w-80 h-72 rounded-lg hover:translate-y-4 ' src={img} alt="" />
+            <div className='border-[#D49B35] mt-2'>
+              <h1>{roomType}</h1>
+              <h1>{roomSize}</h1>
+              <h1>{pricePerNight}</h1>
+            </div>
         </div>
       </Link>
     );
